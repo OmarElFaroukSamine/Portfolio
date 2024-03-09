f92d823fae31657f4160d012ae9837846bcece4c
OmarElFaroukSamine <omarelfarouk.samine@gmail.com>
2024-03-09 19:01:02 +0100

    Almost finished the website, added the skills and experience sections and some main js functions

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var images = [];
 function preload() {
-    for (var i = 0; i < arguments.length; i++) {
-        images[i] = new Image();
-        images[i].src = preload.arguments[i];
-    }
+  for (var i = 0; i < arguments.length; i++) {
+    images[i] = new Image();
+    images[i].src = preload.arguments[i];
+  }
 }
 
 //-- usage --//
 preload(
-    "resources/normal.gif",
-    "resources/first.gif",
-    "resources/last.gif",
-    "resources/reversed.gif"
+  "resources/normal.gif",
+  "resources/first.gif",
+  "resources/last.gif",
+  "resources/reversed.gif"
 
 )
 var firstFrame = "resources/first.gif";
@@ -75,28 +75,129 @@ $("#gif").mouseout(function (e) {
   }
 });
 
-var ssWaypoints = function() {
 
-  var sections = $(".target-section"),
-      navigation_links = $(".header-main-nav li a");
+function navBarColor() {
+  const myNav = $('#nav');
+  const myLi = myNav.find('li');
+  const intro = $('#intro');
+  const about = $('#about');
+  const myHead = $('#head');
+  var aboutBounding = about[0].getBoundingClientRect();
+  var offset = myHead.outerHeight();
+  var introBounding = intro[0].getBoundingClientRect();
 
-  sections.waypoint( {
+  if (introBounding.bottom < offset) {
+    if ($(window).innerWidth() > 871) {
+      myNav.css({
+        'background-color': '#0e131a',
+        'transition': 'background-color 0.5s ease-in-out'
+      });
+      myHead.css({
+        'box-shadow': 'rgb(38, 57, 77) 0px 8px 30px -4px',
+        '-webkit-box-shadow':'rgb(38, 57, 77) 0px 8px 30px -4px',
+        '-moz-box-shadow':'rgb(38, 57, 77) 0px 8px 30px -4px',
+        'transition': 'box-shadow 0.2s ease-in-out'
+      });
+    } else {
+      var anch = myLi.find('a.active');
+      if (anch.length > 0) {
+          anch.css('color', 'white');
+      } else {
+        myLi.find('a').css('color', '#6B7280');
+      }
+    }
+    if (offset > aboutBounding.bottom) {
+      myHead.css({
+        'box-shadow': 'none',
+        'transition': 'box-shadow 0.2s ease-in-out'
+      });
+    }
+    myHead.css('line-height', '70px !important');
+    myLi.each(function () {
+      var anch = $(this).find('a');
+      if (anch.hasClass('active')) {
+        anch.css('color', 'white');
 
-      handler: function(direction) {
-
-          var active_section;
-
-          active_section = $('section#' + this.element.id);
-
-          if (direction === "up") active_section = active_section.prevAll(".target-section").first();
-
-          var active_link = $('.header-main-nav li a[href="#' + active_section.attr("id") + '"]');
-
-          navigation_links.parent().removeClass("current");
-          active_link.parent().addClass("current");
-
-      },
-      offset: '25%'
-
+      } else {
+        anch.css('color', '#6B7280');
+        anch.css('transition', '0ms');
+      }
+      $(this).on('mouseover', function () {
+        $(this).find('a').css('color', 'white');
+      });
+      if (!anch.hasClass('active')) {
+        $(this).on('mouseout', function () {
+          $(this).find('a').css({
+            'color': '#6B7280',
+            'transition': 'color 1s ease-in-out'
+          });
+        });
+      } else {
+        $(this).on('mouseout', function () {
+          $(this).find('a').css('color', 'white');
+        });
+      }
+    });
+  } else {
+    myNav.css('transform', 'none');
+    myNav.css('transition', 'background-color 0.5s ease-in-out');
+    myHead.css('transition', 'box-shadow 0.2s ease-in-out');
+    myHead.css('box-shadow', 'none');
+    myLi.each(function () {
+      var anch = $(this).find('a.active');
+      if (anch.length > 0) {
+        if ($(window).innerWidth() > 871) {
+          myNav.css('background-color', '#d1d3d6');
+          anch.css('color', 'black');
+        } else {
+          myNav.css('background-color', '#0e131a');
+          anch.css('color', 'white');
+        }
+      }
+      $(this).on('mouseover', function () {
+        if ($(window).innerWidth() > 871) {
+          $(this).find('a').css('color', 'black');
+        } else {
+          $(this).find('a').css('color', 'white');
+        }
+      });
+      if (!anch.hasClass('active')) {
+        $(this).find('a').css('color', '#6B7280');
+      }
+    });
+  }
+}
+var addClassOnScroll = function () {
+  var windowTop = $(window).scrollTop();
+  $('section[id]').each(function (index, elem) {
+    var offsetTop = $(elem).offset().top;
+    var outerHeight = $(this).outerHeight(true);
+    if (windowTop > (offsetTop - 50) && windowTop < (offsetTop + outerHeight)) {
+      var elemId = $(elem).attr('id');
+      $("nav ul li a.active").removeClass('active');
+      $("nav ul li a[href='#" + elemId + "']").addClass('active');
+    }
   });
 };
+
+$(document).ready(function () {
+  const scrollContainer = $('.simplebar-content')[0];
+  const simpleBarInstance = new SimpleBar(scrollContainer);
+  $(simpleBarInstance.getScrollElement()).on('scroll', function () { navBarColor(); });
+  $(simpleBarInstance.getScrollElement()).on('scroll', function () { addClassOnScroll(); });
+  $(window).on('resize', function () { navBarColor(); addClassOnScroll();});
+  $('.nava').click(function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href").split("#")[1];
+    if (id){
+      var el = $("#" + id);
+      var container = el.closest('.simplebar-content-wrapper')
+      if (el.length){
+        var scrollTo = el.offset().top - container.offset().top + container.scrollTop();
+        $(container).animate({ scrollTop: scrollTo }, 'slow');
+      }
+    }
+    addClassOnScroll();
+    navBarColor();
+  });
+});
