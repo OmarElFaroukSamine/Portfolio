69d2535195f486464537c51059c7e9793d089d6f
OmarElFaroukSamine <omarelfarouk.samine@gmail.com>
2024-03-11 12:21:29 +0100

    Removing some unnecessary files and organizing the javascript file and trying to optimize it for better performance, also adjusting the education section a little bit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var images = [];
+
 function preload() {
   for (var i = 0; i < arguments.length; i++) {
     images[i] = new Image();
@@ -12,8 +13,8 @@ preload(
   "resources/first.gif",
   "resources/last.gif",
   "resources/reversed.gif"
+);
 
-)
 var firstFrame = "resources/first.gif";
 var lastFrame = "resources/last.gif";
 var normal = "resources/normal.gif";
@@ -31,8 +32,7 @@ function playForward() {
       $("#gif").attr("src", lastFrame);
       playing = false;
     }, animationLength);
-  }
-  else {
+  } else {
     $("#gif").attr("src", firstFrame);
   }
 }
@@ -40,8 +40,7 @@ function playForward() {
 function playBackward() {
   if ($("#gif").is(":hover")) {
     $("#gif").attr("src", lastFrame);
-  }
-  else {
+  } else {
     playing = true;
     $("#gif").attr("src", reverse);
     window.setTimeout(function () {
@@ -56,7 +55,7 @@ $("#gif").mouseover(function (e) {
     checkingIfPlaying = window.setInterval(function () {
       if (!playing) {
         window.clearInterval(checkingIfPlaying);
-        playForward()
+        playForward();
         checkingIfPlaying = false;
       }
     }, 0);
@@ -68,105 +67,93 @@ $("#gif").mouseout(function (e) {
     checkingIfPlaying = window.setInterval(function () {
       if (!playing) {
         window.clearInterval(checkingIfPlaying);
-        playBackward()
+        playBackward();
         checkingIfPlaying = false;
       }
     }, 0);
   }
 });
 
-
 function navBarColor() {
-  const myNav = $('#nav');
-  const myLi = myNav.find('li');
-  const intro = $('#intro');
-  const about = $('#about');
-  const myHead = $('#head');
-  var aboutBounding = about[0].getBoundingClientRect();
-  var offset = myHead.outerHeight();
-  var introBounding = intro[0].getBoundingClientRect();
+  const $myNav = $('#nav');
+  const $myLi = $myNav.find('li');
+  const $intro = $('#intro');
+  const $about = $('#about');
+  const $education = $('#education');
+  const $projects= $('#projects');
+  const $certificate = $('#certificate');
+  const $myHead = $('#head');
+  const offset = $myHead.outerHeight();
+  const introBounding = $intro[0].getBoundingClientRect();
+  const aboutBounding = $about[0].getBoundingClientRect();
+  const projectsBounding = $projects[0].getBoundingClientRect();
+  const educationBounding = $education[0].getBoundingClientRect();
+  const certificateBounding = $certificate[0].getBoundingClientRect();
+  const isWideScreen = $(window).innerWidth() > 871;
 
-  if (introBounding.bottom < offset) {
-    if ($(window).innerWidth() > 871) {
-      myNav.css({
+  if (offset > introBounding.bottom) {
+    if (isWideScreen) {
+      $myNav.css({
         'background-color': '#0e131a',
         'transition': 'background-color 0.5s ease-in-out'
       });
-      myHead.css({
-        'box-shadow': 'rgb(38, 57, 77) 0px 8px 30px -4px',
-        '-webkit-box-shadow':'rgb(38, 57, 77) 0px 8px 30px -4px',
-        '-moz-box-shadow':'rgb(38, 57, 77) 0px 8px 30px -4px',
-        'transition': 'box-shadow 0.2s ease-in-out'
-      });
     } else {
-      var anch = myLi.find('a.active');
-      if (anch.length > 0) {
-          anch.css('color', 'white');
-      } else {
-        myLi.find('a').css('color', '#6B7280');
-      }
+      const $activeAnchor = $myLi.find('a.active');
+      $myLi.find('a').css('color', $activeAnchor.length > 0 ? 'white' : '#6B7280');
     }
-    if (offset > aboutBounding.bottom) {
-      myHead.css({
-        'box-shadow': 'none',
-        'transition': 'box-shadow 0.2s ease-in-out'
+    const isScrolledToSection = offset > aboutBounding.top && aboutBounding.bottom > offset ||
+                                offset > educationBounding.top && educationBounding.bottom > offset ||
+                                offset > certificateBounding.top && certificateBounding.bottom > offset;
+    $myHead.css({
+      'box-shadow': isScrolledToSection ? 'rgb(38, 57, 77) 0px 8px 30px -4px' : 'none',
+      '-webkit-box-shadow': isScrolledToSection ? 'rgb(38, 57, 77) 0px 8px 30px -4px' : 'none',
+      '-moz-box-shadow': isScrolledToSection ? 'rgb(38, 57, 77) 0px 8px 30px -4px' : 'none',
+      'transition': 'box-shadow 0.2s ease-in-out'
+    });
+      $myLi.each(function () {
+      const $anchor = $(this).find('a');
+      const isActive = $anchor.hasClass('active');
+      $anchor.css({
+        color: isActive ? 'white' : '#6B7280',
+        transition: 'color 0.2s ease-in-out'
       });
-    }
-    myHead.css('line-height', '70px !important');
-    myLi.each(function () {
-      var anch = $(this).find('a');
-      if (anch.hasClass('active')) {
-        anch.css('color', 'white');
-
-      } else {
-        anch.css('color', '#6B7280');
-        anch.css('transition', '0ms');
-      }
       $(this).on('mouseover', function () {
-        $(this).find('a').css('color', 'white');
+        $anchor.css('color', 'white');
       });
-      if (!anch.hasClass('active')) {
-        $(this).on('mouseout', function () {
-          $(this).find('a').css({
-            'color': '#6B7280',
-            'transition': 'color 1s ease-in-out'
-          });
-        });
-      } else {
-        $(this).on('mouseout', function () {
-          $(this).find('a').css('color', 'white');
-        });
-      }
+      $(this).on('mouseout', function () {
+        if (!isActive || offset < introBounding.bottom) {
+          $anchor.css('color', '#6B7280');
+        }
+      });
     });
   } else {
-    myNav.css('transform', 'none');
-    myNav.css('transition', 'background-color 0.5s ease-in-out');
-    myHead.css('transition', 'box-shadow 0.2s ease-in-out');
-    myHead.css('box-shadow', 'none');
-    myLi.each(function () {
-      var anch = $(this).find('a.active');
-      if (anch.length > 0) {
-        if ($(window).innerWidth() > 871) {
-          myNav.css('background-color', '#d1d3d6');
-          anch.css('color', 'black');
-        } else {
-          myNav.css('background-color', '#0e131a');
-          anch.css('color', 'white');
-        }
+    $myNav.css({
+      'transform': 'none',
+      'transition': 'background-color 0.5s ease-in-out',
+      'background-color': isWideScreen ? '#d1d3d6' : '#0e131a'
+    });
+    $myHead.css({
+      'transition': 'box-shadow 0.2s ease-in-out',
+      'box-shadow': 'none'
+    });
+    $myLi.each(function () {
+      const $activeAnchor = $(this).find('a.active');
+      const isActive = $activeAnchor.length > 0;
+      if (isActive) {
+        $myNav.css('background-color', isWideScreen ? '#d1d3d6' : '#0e131a');
+        $activeAnchor.css('color', isWideScreen ? 'black' : 'white');
       }
       $(this).on('mouseover', function () {
-        if ($(window).innerWidth() > 871) {
-          $(this).find('a').css('color', 'black');
-        } else {
-          $(this).find('a').css('color', 'white');
-        }
+        $(this).find('a').css('color', isWideScreen ? 'black' : 'white');
       });
-      if (!anch.hasClass('active')) {
+      if (!isActive) {
         $(this).find('a').css('color', '#6B7280');
       }
     });
   }
 }
+
+
 var addClassOnScroll = function () {
   var windowTop = $(window).scrollTop();
   $('section[id]').each(function (index, elem) {
@@ -183,21 +170,30 @@ var addClassOnScroll = function () {
 $(document).ready(function () {
   const scrollContainer = $('.simplebar-content')[0];
   const simpleBarInstance = new SimpleBar(scrollContainer);
-  $(simpleBarInstance.getScrollElement()).on('scroll', function () { navBarColor(); });
-  $(simpleBarInstance.getScrollElement()).on('scroll', function () { addClassOnScroll(); });
-  $(window).on('resize', function () { navBarColor(); addClassOnScroll();});
+  const $scrollElement = $(simpleBarInstance.getScrollElement());
+  const $window = $(window);
+
+
+  function both(){
+    navBarColor();
+    addClassOnScroll();
+  }
+  $scrollElement.on('scroll', both);
+  $window.on('resize', both);
+
+
+  
   $('.nava').click(function (event) {
     event.preventDefault();
     var id = $(this).attr("href").split("#")[1];
-    if (id){
+    if (id) {
       var el = $("#" + id);
       var container = el.closest('.simplebar-content-wrapper')
-      if (el.length){
+      if (el.length) {
         var scrollTo = el.offset().top - container.offset().top + container.scrollTop();
         $(container).animate({ scrollTop: scrollTo }, 'slow');
       }
     }
-    addClassOnScroll();
-    navBarColor();
+  both();
   });
 });
